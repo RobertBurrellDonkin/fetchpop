f8bd4fdb599f850bf8521b5182dc11524359d63d
Robert Burrell Donkin <itstechupnorth@gmail.com>
2018-02-18 19:35:20 +0000

    Added some javadocs.

--- src/main/java/name/robertburrelldonkin/personal/fetchpop/app/IOperation.java
+++ src/main/java/name/robertburrelldonkin/personal/fetchpop/app/IOperation.java
@@ -1,5 +1,9 @@
 package name.robertburrelldonkin.personal.fetchpop.app;
 
+/**
+ * An operation is a series of actions undertaken within the context of a server
+ * session.
+ */
 interface IOperation {
     void operateOn(ISession session);
 }
