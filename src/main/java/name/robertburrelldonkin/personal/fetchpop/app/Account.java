bfa0687fc194e178028924005ccaec91db4b0f7b
Robert Burrell Donkin <107208+RobertBurrellDonkin@users.noreply.github.com>
2018-02-20 19:56:19 +0000

    GH-5 Sketch approach.

--- src/main/java/name/robertburrelldonkin/personal/fetchpop/app/Account.java
+++ src/main/java/name/robertburrelldonkin/personal/fetchpop/app/Account.java
@@ -2,7 +2,10 @@ package name.robertburrelldonkin.personal.fetchpop.app;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 class Account {
 
     private final Logger logger = LoggerFactory.getLogger(Account.class);
@@ -12,12 +15,16 @@ class Account {
     private final String hostName;
     private final int hostPort;
 
-    public Account(final String userName, final String credentials, final String hostName, final int hostPort) {
+    public Account(@Value("${application.user:}") final String userName,
+            @Value("${application.cred:}") final String credentials,
+            @Value("${application.host.name:}") final String hostName,
+            @Value("${application.host.port:-1}") final int hostPort) {
         super();
         this.userName = userName;
         this.credentials = credentials;
         this.hostName = hostName;
         this.hostPort = hostPort;
+        logger.info("Account { user: {}, host: { name: {}, port: {} } }", userName, hostName, hostPort);
     }
 
     public void perform(final Client client, final IOperation operation) {
