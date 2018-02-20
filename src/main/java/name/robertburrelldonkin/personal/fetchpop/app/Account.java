482b05cb38507fed617d59ea0bae2465b5891145
Robert Burrell Donkin <107208+RobertBurrellDonkin@users.noreply.github.com>
2018-02-20 21:25:19 +0000

    GH-5 Minor security improvement
    Store credentials in char array, reducing risk
    that they will be printed toString
    
    Fixes #5

--- src/main/java/name/robertburrelldonkin/personal/fetchpop/app/Account.java
+++ src/main/java/name/robertburrelldonkin/personal/fetchpop/app/Account.java
@@ -1,6 +1,8 @@
 package name.robertburrelldonkin.personal.fetchpop.app;
 
+import static java.util.Objects.nonNull;
 import static org.apache.commons.codec.digest.DigestUtils.sha1Hex;
+import static org.apache.commons.lang.ArrayUtils.EMPTY_CHAR_ARRAY;
 import static org.apache.commons.lang.StringUtils.isBlank;
 
 import org.slf4j.Logger;
@@ -14,7 +16,11 @@ class Account {
     private final Logger logger = LoggerFactory.getLogger(Account.class);
 
     private final String userName;
-    private final String credentials;
+    /**
+     * Minor security gain by reducing risk that password is accidentally
+     * printed to String
+     */
+    private final char[] credentials;
     private final String hostName;
     private final int hostPort;
 
@@ -24,33 +30,38 @@ class Account {
             @Value("${application.host.port:995}") final int hostPort) {
         super();
         this.userName = userName;
-        this.credentials = credentials;
+        this.credentials = nonNull(credentials) ? credentials.toCharArray().clone()
+                /* cloning prevent concurrent zeroing */ : EMPTY_CHAR_ARRAY;
         this.hostName = hostName;
         this.hostPort = hostPort;
         logger.info("Account { user: {}, host: { name: {}, port: {} } }", userName, hostName, hostPort);
     }
 
     public String digestCredentialsAsSHA() {
-        return sha1Hex(credentials);
+        return sha1Hex(credentialsAsString());
+    }
+
+    private String credentialsAsString() {
+        return new String(this.credentials);
     }
 
     public String getUserName() {
-        return userName;
+        return this.userName;
     }
 
     public String getHostName() {
-        return hostName;
+        return this.hostName;
     }
 
     public int getHostPort() {
-        return hostPort;
+        return this.hostPort;
     }
 
     public void perform(final Client client, final IOperation operation) {
         logger.info("Opening connection to perform {}", operation);
         client.connect(this.hostName, this.hostPort);
         try {
-            client.login(this.userName, this.credentials);
+            client.login(this.userName, credentialsAsString());
             try {
                 operation.operateOn(client.verify());
             } finally {
@@ -63,7 +74,12 @@ class Account {
     }
 
     public boolean isEmpty() {
-        return isBlank(userName) || isBlank(credentials) || isBlank(hostName) || hostPort <= 0;
+        return isBlank(userName) || isBlank(credentialsAsString()) || isBlank(hostName) || hostPort <= 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Account [userName=" + userName + ", hostName=" + hostName + ", hostPort=" + hostPort + "]";
     }
 
 }
