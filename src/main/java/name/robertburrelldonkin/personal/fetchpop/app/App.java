3de902fa73058b80d4b23e10d00711e055424081
Robert Burrell Donkin <107208+RobertBurrellDonkin@users.noreply.github.com>
2018-02-20 20:08:59 +0000

    GH-5 Revert cruft introduced.

--- src/main/java/name/robertburrelldonkin/personal/fetchpop/app/App.java
+++ src/main/java/name/robertburrelldonkin/personal/fetchpop/app/App.java
@@ -3,36 +3,12 @@ package name.robertburrelldonkin.personal.fetchpop.app;
 import static org.springframework.boot.SpringApplication.exit;
 import static org.springframework.boot.SpringApplication.run;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
 
-    private final Logger logger = LoggerFactory.getLogger(App.class);
-
     public static void main(String[] args) {
         System.exit(exit(run(App.class, args)));
     }
-
-    @Bean
-    public ApplicationRunner applicationRunner() {
-        return args -> {
-            // TODO: TDD
-            // Account account = new Account(userName, credentials, hostName,
-            // hostPort);
-            // args.getNonOptionArgs().stream().forEachOrdered(op -> {
-            // switch (op) {
-            // case "status":
-            // account.perform(new Client(), new
-            // PrintStatusOperation(System.out));
-            // break;
-            // }
-            // });
-        };
-    }
-
 }
