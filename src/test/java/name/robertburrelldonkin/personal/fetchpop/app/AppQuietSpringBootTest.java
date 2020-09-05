3e5202a9912edb71753a0a9907d317700695c0c9
Robert Burrell Donkin <107208+RobertBurrellDonkin@users.noreply.github.com>
2020-09-05 10:43:03 +0100

    GH-28: Upgrade deprecated dependencies.

--- src/test/java/name/robertburrelldonkin/personal/fetchpop/app/AppQuietSpringBootTest.java
+++ src/test/java/name/robertburrelldonkin/personal/fetchpop/app/AppQuietSpringBootTest.java
@@ -1,24 +1,23 @@
 package name.robertburrelldonkin.personal.fetchpop.app;
 
-import static name.robertburrelldonkin.personal.fetchpop.app.AlphaSequence.nextAlphanumeric;
-import static name.robertburrelldonkin.personal.fetchpop.app.StandardOutput.STDERR_MARKER;
-import static name.robertburrelldonkin.personal.fetchpop.app.StandardOutput.STDOUT_MARKER;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.isEmptyString;
-import static org.junit.Assert.assertThat;
-
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.contrib.java.lang.system.SystemErrRule;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MarkerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.rule.OutputCapture;
+import org.springframework.boot.test.system.OutputCaptureRule;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static name.robertburrelldonkin.personal.fetchpop.app.AlphaSequence.nextAlphanumeric;
+import static name.robertburrelldonkin.personal.fetchpop.app.StandardOutput.STDERR_MARKER;
+import static name.robertburrelldonkin.personal.fetchpop.app.StandardOutput.STDOUT_MARKER;
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -28,10 +27,7 @@ public class AppQuietSpringBootTest {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     @Rule
-    public final SystemErrRule systemErrCapture = new SystemErrRule().enableLog();
-
-    @Rule
-    public OutputCapture capture = new OutputCapture();
+    public OutputCaptureRule output = new OutputCaptureRule();
 
     private String someMessage;
 
@@ -43,18 +39,18 @@ public class AppQuietSpringBootTest {
     @Test
     public void quietProfileIsQuiet() {
         logger.info(MarkerFactory.getMarker("not.out"), someMessage);
-        assertThat(capture.toString(), isEmptyString());
+        assertThat(output.toString(), is(emptyString()));
     }
 
     @Test
     public void quietProfilePrintsMarkedMessages() {
         logger.info(STDOUT_MARKER, someMessage);
-        assertThat(capture.toString(), is(someMessage + "\n"));
+        assertThat(output.toString(), is(someMessage + "\n"));
     }
 
     @Test
     public void quietProfilePrintsErrMarkedMessages() {
         logger.info(STDERR_MARKER, someMessage);
-        assertThat(systemErrCapture.getLog(), is(someMessage + "\n"));
+        assertThat(output.toString(), is(someMessage + "\n"));
     }
 }
